5896526abc1c7e014577f5c853d62d25afe8efc0
Алмат Амирсеитов <amirseit.a.a@gmail.com>
2022-11-18 16:17:37 +0600

    feat:Connecting the configuration file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alma-amirseitov/TgBot
 
 go 1.19
+
+require github.com/BurntSushi/toml v1.2.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
-package Main
+package main
+
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"log"
+)
+
+type Config struct {
+	Env string
+}
 
 func main() {
+	configPath := flag.String("config", "", "Path to config file")
+	flag.Parse()
 
+	cfg := &Config{}
+	_, err := toml.DecodeFile(*configPath, cfg)
+
+	if err != nil {
+		log.Fatalf("Ошибка декодирования файла конфигов %v", err)
+	}
+
+	log.Println(cfg.Env)
 }
